16cab22e1c883d17952a90df92ad3b69fd581682
Nick Scilingo <59902216+focusedonsound@users.noreply.github.com>
2020-03-29 16:43:19 -0400

    a few changes
    here we go

--- config.php
+++ config.php
@@ -1,29 +1,49 @@
-<?php
-if(isset($_POST['btn-upload']))
-{
-	$pic = rand(1000,100000)."-".$_FILES['pic']['name'];
-    $pic_loc = $_FILES['pic']['tmp_name'];
-	$folder="uploaded_files/";
-	if(move_uploaded_file($pic_loc,$folder.$pic))
-	{
-		?><script>alert('successfully uploaded');</script><?php
-	}
-	else
-	{
-		?><script>alert('error while uploading file');</script><?php
-	}
-}
-?>
-<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Simpel File Uploading With PHP</title>
-</head>
-<body>
-<form action="" method="post" enctype="multipart/form-data">
-<input type="file" name="pic" />
-<button type="submit" name="btn-upload">upload</button>
-</form>
-</body>
+<!--
+  Author: Abhinav Singh
+  Written on: 23th May, 2008
+  Version: 1
+  Last Edited: 30th May, 2008
+  Contact: admin@abhinavsingh.com
+  License: Creative Commons Attribution-Share Alike 2.5 India License
+-->
+<html>
+  <head>
+    <script type="text/javascript">
+      // Javascript function which takes care for multiple uploads
+      var attachmentlimit = 5; // Limiting maximum uploads to 5
+      var attachmentid = 1;
+      function attachmore() { // Function is called when user presses Attach Another File
+        attachmentid += 1;
+        document.getElementById('attachmentdiv').innerHTML += '<div id="attachmentdiv_' + attachmentid + '" style="margin-top:5px"><input type="file" id="attachment_' + attachmentid + '" name="attachment_' + attachmentid + '" size="30" onchange="document.uploadattachments.submit();"/></div>';
+        if(attachmentid == attachmentlimit) {
+          document.getElementById('addanother').style.display='none';
+        }
+      }
+    </script>
+  </head>
+  <body>
+    <div style="margin-left:10px">
+      <h1>Upload files</h1>
+    </div>
+    <!-- Form taking care of the uploads, notice that the frame target is the iframe contained inside, to which it fires upload.php -->
+    <form id="uploadattachments" enctype="multipart/form-data" name="uploadattachments" target="attachmentiframe" action="upload.php" method="post">
+      <div id="attachmentdiv" style="margin-left:30px">
+        <iframe name="attachmentiframe" style="display:none"></iframe>
+        <div id="attachmentdiv_1" style="margin-top:5px">
+            <input type="file" id="attachment_1" name="attachment_1" size="30" onchange="document.uploadattachments.submit();"/>
+        </div>
+      </div>
+      <!-- div showing error message for invalid file type -->
+      <div id="typeerrormessage" style="display:none;margin-left:30px">
+        <font color=#990000 size=1>Only png, jpg and gif file type are supported</font>
+      </div>
+      <!-- div showing error message for exceeded file size -->
+      <div id="sizeerrormessage" style="display:none;margin-left:30px">
+        <font color=#990000 size=1>File exceeded maximum allowed limit of 100 Kb</font>
+      </div>
+      <div id="addanother" style="margin-left:30px;margin-top:5px">
+        <a href="javascript:void(0)" onclick="attachmore();"><font size=2>Attach another file</font></a>
+      </div>
+    </form>
+  </body>
 </html>
--- upload.php
+++ upload.php
@@ -1,17 +1,48 @@
+<!--
+  Author: Abhinav Singh
+  Written on: 23th May, 2008
+  Version: 1
+  Last Edited: 30th May, 2008
+  Contact: admin@abhinavsingh.com
+  License: Creative Commons Attribution-Share Alike 2.5 India License
+-->
 <?php
-$target_dir = "/opt/fpp/www/css/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
+  // Set the upload target directory
+  $target_path = "upload/";
+
+  for($i=1;$i<=5;$i++) {
+    $attachments = 'attachment_'.$i;
+    $attachmentdiv = 'attachmentdiv_'.$i;
+    $FileName = $_FILES[$attachments]['name'];
+    // Check if filename is empty
+    if($FileName != "") {
+      $FileType = $_FILES[$attachments]['type'];
+      $FileExtension = strtolower(substr($FileName,strrpos($FileName,'.')+1));
+      // Check for supported file formats
+      if($FileExtension != "css" && $FileExtension != "jpg" && $FileExtension != "png") {
+        echo "<script type='text/javascript'>parent.document.getElementById('typeerrormessage').style.display = 'inline';</script>";
+      }
+      else {
+        $FileSize = round($_FILES[$attachments]['size']/1024);
+        // Check for file size
+        if($FileSize > 100) {
+          echo "<script type='text/javascript'>parent.document.getElementById('sizeerrormessage').style.display = 'inline';</script>";
+        }
+        else {
+          $FileTemp = $_FILES[$attachments]['tmp_name'];
+          $FileLocation = $target_path.basename($FileName);
+          // Finally Upload
+          if(move_uploaded_file($FileTemp,$FileLocation)) {
+            // On successful upload send a message to corresponding attachmentdiv from which the file came from
+            echo "<script type='text/javascript'>parent.document.getElementById('".$attachmentdiv."').innerHTML = '<input CHECKED type=\"checkbox\"><a href=\"http://abhinavsingh.com/webdemos/upload/".$FileName."\" target=\"_blank\"><font size=2><b>".$FileName."</b> <i>(".$FileType.")</i> ".$FileSize." Kb</font>';</script>";
+            echo "<script type='text/javascript'>parent.document.getElementById('typeerrormessage').style.display = 'none';</script>";
+            echo "<script type='text/javascript'>parent.document.getElementById('sizeerrormessage').style.display = 'none';</script>";
+          } 
+          else {
+            echo "There was an error uploading the file, please try again!";
+          }
+        }
+      }
     }
-}
+  }
 ?>
