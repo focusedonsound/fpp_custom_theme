b45c85b5a9e2c2bf2038844aa9f7163976711d10
Nick Scilingo <59902216+focusedonsound@users.noreply.github.com>
2020-03-29 17:43:40 -0400

    Update config.php

--- config.php
+++ config.php
@@ -18,7 +18,7 @@
       }
       
       if(empty($errors)==true) {
-         move_uploaded_file($file_tmp,"uploads/".$file_name);
+         move_uploaded_file($file_tmp,"/opt/fpp/www/css/".$file_name);
          echo "Success";
       }else{
          print_r($errors);
