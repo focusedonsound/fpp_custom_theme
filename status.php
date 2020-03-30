ce5c47b643b03959c1923669f8a509b3417f2683
Nick Scilingo <59902216+focusedonsound@users.noreply.github.com>
2020-03-30 14:46:43 -0400

    Update status.php

--- status.php
+++ status.php
@@ -1,7 +1,11 @@
-<p>&nbsp;</p>
-<p>Attributes applied in custom.css</p>
-<p>&nbsp;</p>
-<div id="list">
-  <p><iframe src="/api/configfile/custom.css" frameborder="0" height="400"
-      width="95%"></iframe></p>
-</div>
+<table>
+<tbody>
+<tr>
+<td><h1>Custom Theme - Attributes applied in custom.css</h1></td>
+</tr>
+<tr>
+<td><iframe src="/api/configfile/custom.css" frameborder="0" height="400"
+      width="95%"></iframe></td>
+</tr>
+</tbody>
+</table>
