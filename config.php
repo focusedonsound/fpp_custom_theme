cfc351930636c25fbdbb06bb31f4f2c066122012
Nick Scilingo <59902216+focusedonsound@users.noreply.github.com>
2020-03-30 16:16:06 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,3 +1,72 @@
+.ctsHeader {
+font-family: futura;
+      font-style: italic;    
+      width:100%;
+      margin: 0 auto;
+      text-align: center;
+      color:#313131;
+      -webkit-animation:colorchange 20s infinite alternate;
+      
+      
+    }
+
+    @-webkit-keyframes colorchange {
+      0% {
+        
+        color: blue;
+      }
+      
+      10% {
+        
+        color: #8e44ad;
+      }
+      
+      20% {
+        
+        color: #1abc9c;
+      }
+      
+      30% {
+        
+        color: #d35400;
+      }
+      
+      40% {
+        
+        color: blue;
+      }
+      
+      50% {
+        
+        color: #34495e;
+      }
+      
+      60% {
+        
+        color: blue;
+      }
+      
+      70% {
+        
+        color: #2980b9;
+      }
+      80% {
+     
+        color: #f1c40f;
+      }
+      
+      90% {
+     
+        color: #2980b9;
+      }
+      
+      100% {
+        
+        color: pink;
+      }
+    }
+}
+</style>
 <?php
    if(isset($_FILES['text'])){
       $errors= array();
@@ -27,19 +96,23 @@
 ?>
 <html>
    <body>
-    <table>
+    <table class="ctstable">
 <tbody>
+<tr>
+<td class="ctsHeader">Custom Theme</td>
+<td class="ctsHeader">Upload Your custom.css or custom.js files.</td>
+</tr>
 <tr>
 <td>      <form action = "" method = "POST" enctype = "multipart/form-data">
          <input type = "file" name = "text" /></td>
-<td>         <ul>
+<td> <input type = "submit"/></td>
+</tr>
+<tr>
+<td><ul>
             <li>Sent file: <?php echo $_FILES['text']['name'];  ?>
             <li>File size: <?php echo $_FILES['text']['size'];  ?>
             <li>File type: <?php echo $_FILES['text']['type'] ?>
          </ul></td>
-</tr>
-<tr>
-<td><input type = "submit"/></td>
 
 </tr>
 
