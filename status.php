807d15fdf2cb5ec5bd35e45bb19569d0646b2f70
Nick Scilingo <59902216+focusedonsound@users.noreply.github.com>
2020-03-30 15:22:52 -0400

    Update status.php

--- status.php
+++ status.php
@@ -1,10 +1,86 @@
+<style>
+.ctsHeader {
+ font-family: futura;
+      font-style: italic;
+      
+      width:100%;
+      
+      margin: 0 auto;
+      text-align: center;
+      
+      color:#313131;
+      font-size:45px;
+      font-weight: bold;
+      position: absolute;
+      -webkit-animation:colorchange 20s infinite alternate;
+      
+      
+    }
+
+    @-webkit-keyframes colorchange {
+      0% {
+        
+        color: blue;
+      }
+      
+      10% {
+        
+        color: #8e44ad;
+      }
+      
+      20% {
+        
+        color: #1abc9c;
+      }
+      
+      30% {
+        
+        color: #d35400;
+      }
+      
+      40% {
+        
+        color: blue;
+      }
+      
+      50% {
+        
+        color: #34495e;
+      }
+      
+      60% {
+        
+        color: blue;
+      }
+      
+      70% {
+        
+        color: #2980b9;
+      }
+      80% {
+     
+        color: #f1c40f;
+      }
+      
+      90% {
+     
+        color: #2980b9;
+      }
+      
+      100% {
+        
+        color: pink;
+      }
+    }
+}
+</style>
 <table>
 <tbody>
 <tr>
-<td><h1>Custom Theme - Attributes applied in custom.css</h1></td>
+<td class="ctsHeader"><h1>Custom Theme </br> Attributes applied in custom.css</h1></td>
 </tr>
 <tr>
-<td><iframe src="/api/configfile/custom.css" frameborder="0" height="400"
+<td class="ctscontent"><iframe src="/api/configfile/custom.css" frameborder="0" height="400"
       width="95%"></iframe></td>
 </tr>
 </tbody>
